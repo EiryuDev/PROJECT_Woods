0c00e1994a7603cb54e61d6bfd4081d4bd34b96c
Adnan Yousuf <adnanxyousuf@gmail.com>
2025-06-10 19:00:55 +0530

    PLAYER: Created weapon manager | v0.1

---
+++ Scripts/Combat/Player/PlayerWeaponManager.gd
@@ -0,0 +1,47 @@
+extends Node3D
+
+@onready var weapons = $Weapons.get_children()
+var weaponsUnlocked = []
+var currentSlot = 0
+var currentWeapon = null
+
+func _ready():
+	DisableAllWeapons()
+	for _i in range(weapons.size()):
+		weaponsUnlocked.append(false) # True for testing, default false
+	pass
+	
+func DisableAllWeapons():
+	for weapon in weapons:
+		if has_method("set_active"):
+			weapon.set_active(false)
+		else:
+			weapon.hide()
+
+func SwitchToPreviousWeapon():
+	for i in range(weapons.size()):
+		var wrappedIndex = wrapi(currentSlot - 1 - i, 0, weapons.size())
+		if SwitchToWeaponSlot(wrappedIndex):
+			break
+	
+func SwitchToNextWeapon():
+	for i in range(weapons.size()):
+		var wrappedIndex = wrapi(currentSlot + 1 + i, 0, weapons.size())
+		if SwitchToWeaponSlot(wrappedIndex):
+			break
+
+func SwitchToWeaponSlot(slotIndex: int)->bool:
+	if slotIndex >= weapons.size() or slotIndex < 0:
+		return false
+	if weaponsUnlocked.size() == 0 or !weaponsUnlocked[slotIndex]:
+		return false
+		
+	DisableAllWeapons()
+	currentSlot = slotIndex
+	currentWeapon = weapons[currentSlot]
+	if has_method("set_active"):
+		currentWeapon.set_active(true)
+	else:
+		currentWeapon.show()
+		
+	return true
--- Scripts/_Root/Player/PlayerManager.gd
+++ Scripts/_Root/Player/PlayerManager.gd
@@ -2,12 +2,39 @@ extends Node
 
 @onready var playerStatsManager = $"Player Stats Manager"
 @onready var playerLocomotionManager = $"Player Locomotion Manager"
+@onready var playerWeaponManager = $"Camera3D/Player Weapon Manager"
 
+const HOTKEYS = {
+	KEY_1:0,
+	KEY_2:1,
+	KEY_3:2,
+	KEY_4:3,
+	KEY_5:4,
+	KEY_6:5,
+	KEY_7:6,
+	KEY_8:7,
+	KEY_9:8,
+	KEY_0:9,
+}
 var isDead = false
 
 func _ready():
 	playerStatsManager.died.connect(kill)
+
+func _input(event):
+	if isDead:
+		return
 	
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			playerWeaponManager.SwitchToPreviousWeapon()
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			playerWeaponManager.SwitchToNextWeapon()
+	
+	if event is InputEventKey and event.pressed and event.keycode in HOTKEYS:
+		playerWeaponManager.SwitchToWeaponSlot(HOTKEYS[event.keycode])
+	
+
 func kill():
 	isDead = true;
 	playerLocomotionManager.StopMovement()
